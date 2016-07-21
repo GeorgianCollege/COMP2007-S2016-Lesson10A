35af5fe57aab7e8989e5a4a99febd3fd6be06d54
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-21 09:24:05 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson10A/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_S2016_Lesson10A.Startup))]
+namespace COMP2007_S2016_Lesson10A
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
