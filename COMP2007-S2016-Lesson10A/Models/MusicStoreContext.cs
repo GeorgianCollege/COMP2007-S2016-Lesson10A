2fd4aae7d1e9c6ff595d8d8e4fc486f2b34449ef
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-21 11:26:48 -0400

    updated storecontroller and added MusicStoreConnection to Web.config - Fixes Issue #6

--- COMP2007-S2016-Lesson10A/Models/MusicStoreContext.cs
+++ COMP2007-S2016-Lesson10A/Models/MusicStoreContext.cs
@@ -8,6 +8,12 @@ namespace COMP2007_S2016_Lesson10A.Models
 {
     public class MusicStoreContext: DbContext
     {
+        public MusicStoreContext():
+            base("name=MusicStoreConnection")
+        {
+
+        }
+
         public virtual DbSet<Album> Albums { get; set; }
         public virtual DbSet<Genre> Genres { get; set; }
         public virtual DbSet<Artist> Artists { get; set; }
