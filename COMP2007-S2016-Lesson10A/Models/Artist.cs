b4b4f8673dd3c699aec3ea863debc3011397e2d8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-21 10:33:01 -0400

    Added Artist Model - Fixes #1

---
+++ COMP2007-S2016-Lesson10A/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_S2016_Lesson10A.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
